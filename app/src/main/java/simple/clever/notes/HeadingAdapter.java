fbddce9e24cf1f258108e482c28978a54b8a6946
iBekas <s345553452S>
2021-07-07 22:48:58 +0300

    Добавил адаптер.

--- app/src/main/java/simple/clever/notes/HeadingAdapter.java
+++ app/src/main/java/simple/clever/notes/HeadingAdapter.java
@@ -1,4 +1,53 @@
 package simple.clever.notes;
 
-public class HeadingAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HeadingAdapter extends RecyclerView.Adapter<HeadingAdapter.ViewHolder> {
+
+    private String[] noteHead;
+
+    public HeadingAdapter(String[] noteHead) {
+        this.noteHead = noteHead;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HeadingAdapter.ViewHolder holder, int position) {
+    holder.getTextHead().setText(noteHead[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return noteHead.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textHead;
+        private TextView textTime;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+//            textHead = (TextView) textHead;
+        }
+
+        public TextView getTextHead() {
+            return textHead;
+        }
+
+        public TextView getTextTime() {
+            return textTime;
+        }
+    }
 }
