31732139f66677e1cfa82abdade1171251894b2e
iBekas <s345553452S>
2021-07-08 13:47:03 +0300

    Добвил класс данных - содержимое заголовка заметки.

--- app/src/main/java/simple/clever/notes/CardData.java
+++ app/src/main/java/simple/clever/notes/CardData.java
@@ -1,4 +1,19 @@
 package simple.clever.notes;
 
 public class CardData {
+    private String head;
+    private String timeOpen;
+
+    public CardData(String head, String timeOpen) {
+        this.head = head;
+        this.timeOpen = timeOpen;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public String getTimeOpen() {
+        return timeOpen;
+    }
 }
