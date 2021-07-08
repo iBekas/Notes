7850a0bb64171056e889d18bb432c28164983998
iBekas <s345553452S>
2021-07-08 15:32:35 +0300

    Создал класс для парсеализации.

---
+++ app/src/main/java/simple/clever/notes/Note.java
@@ -0,0 +1,34 @@
+package simple.clever.notes;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Note implements Parcelable {
+
+
+
+
+    protected Note(Parcel in) {
+    }
+
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+    }
+}
